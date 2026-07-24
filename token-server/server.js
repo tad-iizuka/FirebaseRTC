6ee09def52c607b1114c59752a69ef48bc9a5f0b
tad-iizuka <tad.iizuka@gmail.com>
2026-07-24 23:46:51 +0900

    fix

--- token-server/server.js
+++ token-server/server.js
@@ -80,7 +80,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Vary', 'Origin');
   }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
