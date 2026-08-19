6998c538079d2ca784f00a103f23a487079a3797
tad-iizuka <tad.iizuka@gmail.com>
2026-08-19 17:51:07 +0900

    update

--- token-server/server.js
+++ token-server/server.js
@@ -95,7 +95,7 @@ app.use((req, res, next) => {
     res.setHeader('Vary', 'Origin');
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, X-Firebase-AppCheck');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
